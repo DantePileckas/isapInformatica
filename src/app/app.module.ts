9c933036ddfd3fad18468e3d687693f145c3ac7a
DantePileckas <pileckas33@gmail.com>
2022-07-18 02:08:06 -0300

    Estructura del inicio, manejo de componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { MaterialesComponent } from './components/materiales/materiales.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+//Material Module
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
-  ],
+    HomeComponent,
+    CursosComponent,
+    MaterialesComponent ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatGridListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
